24233e55381d431390dfa4f7ebe16749e3067d23
Jaime Nguyen <jaimenguyen168@gmail.com>
2025-03-19 18:47:34 -0400

    Sanity config all types

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,24 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { studentType } from "@/sanity/schemaTypes/studentType";
+import { courseType } from "@/sanity/schemaTypes/courseType";
+import { instructorType } from "@/sanity/schemaTypes/instructorType";
+import { categoryType } from "@/sanity/schemaTypes/categoryType";
+import { moduleType } from "@/sanity/schemaTypes/moduleType";
+import { lessonType } from "@/sanity/schemaTypes/lessonType";
+import { enrollmentType } from "@/sanity/schemaTypes/enrollmentType";
+import { lessonCompletionType } from "@/sanity/schemaTypes/lessonCompletionType";
+import { blockContent } from "@/sanity/schemaTypes/blockContent";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [studentType],
+  types: [
+    studentType,
+    courseType,
+    instructorType,
+    categoryType,
+    moduleType,
+    lessonType,
+    enrollmentType,
+    lessonCompletionType,
+    blockContent,
+  ],
 };
