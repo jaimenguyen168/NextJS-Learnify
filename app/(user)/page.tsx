e64e30796b73be6c09c63b134f1b8ae98f3dba9e
Jaime Nguyen <jaimenguyen168@gmail.com>
2025-03-19 17:12:57 -0400

    Hero

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import Hero from "@/app/(user)/Hero";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home
+    <div className="min-h-screen bg-background">
+      <Hero />
+      <div className="container mx-auto px-4">
+        <div className="flex items-center gap-4 py-8">
+          <div className="h-px flex-1 bg-gradient-to-r from-border/0 via-border to-border/0" />
+          <span className="text-sm font-medium text-muted-foreground">
+            Featured Courses
+          </span>
+          <div className="h-px flex-1 bg-gradient-to-r from-border/0 via-border to-border/0" />
+        </div>
+      </div>
     </div>
   );
 }
